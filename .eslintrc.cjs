e0ea280ee61dd64e055103171c6c8a3a1b761b4b
José Teles <josetelesmaciel@gmail.com>
2025-10-27 06:01:38 -0300

    chore: add eslint and prettier configuration
    - Add ESLint config with TypeScript and Prettier integration
    - Add Prettier config with project code style standards
    - Add .prettierignore for build artifacts and dependencies

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', 'node_modules', '*.config.js', 'vitest.config.ts'],
+  rules: {},
+};
