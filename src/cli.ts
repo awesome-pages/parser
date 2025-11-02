57df9ee460ae7c788f2af9968f223960fa1ca02f
José Teles <josetelesmaciel@gmail.com>
2025-11-02 07:32:13 -0300

    chore(cli): update input to test HTTP source
    Change input to a gist URL for testing the new HTTP raw source functionality.
    
    Related to #5

--- src/cli.ts
+++ src/cli.ts
@@ -5,7 +5,9 @@ import { mdastToDomain } from '@/core/mdastToDomain.js';
 import { createSource } from '@/sources/createSource';
 
 // const input = process.argv[2] || 'README.md';
-const input = 'github://teles/awesome-click-and-use@main:README.md';
+// const input = 'github://teles/awesome-click-and-use@main:README.md';
+const input =
+  'https://gist.githubusercontent.com/teles/f045f2866245016b4945829938ef03bf/raw/c977dff9bb624723b12cf8cd6d7785e41bcb9074/teste.md';
 
 (async () => {
   const source = createSource(input);
